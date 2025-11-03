f3393bf0fb81aa04e8a06aa4030befc9b792271a
hvppycoding <hvppycoding@gmail.com>
2025-11-04 01:02:24 +0900

    HFDebug 노드의 메시지 처리 및 위젯 업데이트 로직 개선

--- web/debug_nodes.js
+++ web/debug_nodes.js
@@ -7,19 +7,19 @@ app.registerExtension({
         if (nodeData.name === "HFDebug") {
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
-                console.log("HFDebug - message:", message);
-                console.log("HFDebug - this", this);
                 onExecuted?.apply(this, arguments);
 
-                if (this.widgets) {
-					for (let i = 0; i < this.widgets.length; i++) {
-						this.widgets[i].onRemove?.();
-					}
-					this.widgets.length = 0;
-				}
+                this.properties.text = message["text"]
 
-                // Check if the "text" widget already exists.
+                let comboWidget = this.widgets && this.widgets.find(w => w.name === "displayindex");
                 let textWidget = this.widgets && this.widgets.find(w => w.name === "displaytext");
+                if (!comboWidget) {
+                    comboWidget = this.addWidget("combo", "displayindex", 0, () => {
+                        comboWidget.value = Math.min(comboWidget.value, comboWidget.options.values.length - 1);
+                        textWidget.value = message["text"]?.[comboWidget.value];       
+                    }, {values: Array.from({ length: message["text"].length }, (_, i) => i)});
+                    comboWidget.value = 0;
+                } 
                 if (!textWidget) {
                     textWidget = ComfyWidgets["STRING"](this, "displaytext", ["STRING", { multiline: true }], app).widget;
                     textWidget.inputEl.readOnly = true;
@@ -28,7 +28,56 @@ app.registerExtension({
                     textWidget.serialize = false;
                     textWidget.options.serialize = false; // Prevent prompt serialization
                 }
-                textWidget.value = message["text"].join("");
+                
+                if (comboWidget.options.values.length !== message["text"].length) {
+                    comboWidget.options.values = Array.from({ length: message["text"].length }, (_, i) => i);
+                    comboWidget.value = Math.min(comboWidget.value, comboWidget.options.values.length - 1);
+                }
+                textWidget.value = message["text"]?.[comboWidget.value];
+
+                comboWidget.onChange = (data) => {
+                    console.log("Combo changed" + comboWidget.value);
+                    const index = comboWidget.value;
+                    textWidget.value = message["text"]?.[index];
+                };
+            };
+
+            const onConfigure = nodeType.prototype.onConfigure;
+            nodeType.prototype.onConfigure = function () {
+                onConfigure?.apply(this, arguments);
+                
+                let comboWidget = this.widgets && this.widgets.find(w => w.name === "displayindex");
+                let textWidget = this.widgets && this.widgets.find(w => w.name === "displaytext");
+                if (!comboWidget) {
+                    comboWidget = this.addWidget("combo", "displayindex", 0, () => {
+                        comboWidget.value = Math.min(comboWidget.value, comboWidget.options.values.length - 1);
+                        textWidget.value = this.properties?.text?.[comboWidget.value];
+                    }, {values: Array.from({ length: this.properties?.text.length ?? 0 }, (_, i) => i)});
+                    comboWidget.value = 0;
+                }
+                if (!textWidget) {
+                    textWidget = ComfyWidgets["STRING"](this, "displaytext", ["STRING", { multiline: true }], app).widget;
+                    textWidget.inputEl.readOnly = true;
+                    textWidget.inputEl.style.border = "none";
+                    textWidget.inputEl.style.backgroundColor = "transparent";
+                    textWidget.serialize = false;
+                    textWidget.options.serialize = false; // Prevent prompt serialization
+                }
+
+                if (comboWidget.options.values.length !== this.properties?.text.length) {
+                    comboWidget.options.values = Array.from({ length: this.properties?.text.length }, (_, i) => i);
+                    comboWidget.value = Math.min(comboWidget.value, comboWidget.options.values.length - 1);
+                }
+
+                comboWidget.onChange = (data) => {
+                    console.log("Combo changed" + comboWidget.value);
+                    const index = comboWidget.value;
+                    textWidget.value = this.properties?.text?.[index];
+                };
+
+                if (this.properties?.text) {
+                    textWidget.value = this.properties?.text?.[comboWidget.value];
+                }
             };
         }
     },
